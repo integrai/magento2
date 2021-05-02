9faebb1b10faba9d7211c2a8415b1bd44fd1b05c
Eric Barbosa <ericcarmobarbosa@gmail.com>
2021-05-02 19:13:55 -0300

    fix abandoned_cart_item event send

--- Cron/AbandonedCart.php
+++ Cron/AbandonedCart.php
@@ -43,44 +43,14 @@ class AbandonedCart
               $minutes = $this->_getHelper()->getGlobalConfig('minutesAbandonedCartLifetime', 60);
               $fromDate = date('Y-m-d H:i:s', strtotime('-'.$minutes. ' minutes'));
               $toDate = date('Y-m-d H:i:s', strtotime("now"));
-
-//              $fromDate = date('Y-m-d H:i:s', strtotime('-'.$minutes. ' minutes'));
-//              $toDate = date('Y-m-d H:i:s', strtotime("now"));
-
-              $allQuotes = $this->_quoteFactory
-                  ->create()
-                  ->getCollection()
-//                  ->addFieldToFilter('items_count', array('gt' => 0))
-//                  ->addFieldToFilter('customer_email', array('notnull' => true))
-                  ->load();
-
-              foreach ($allQuotes as $quote) {
-                $this->_getHelper()->log('==> $allQuotes', $quote->getData('created_at'));
-              }
-
-              $activeQuotes = $this->_quoteFactory
-                  ->create()
-                  ->getCollection()
-                  ->addFieldToFilter('is_active', 1)
-                  ->addFieldToFilter('items_count', array('gt' => 0))
-                  ->addFieldToFilter('customer_email', array('notnull' => true))
-                  ->load();
-
-              foreach ($activeQuotes as $quote) {
-                $this->_getHelper()->log('==> $activeQuotes', $quote->getData('created_at'));
-              }
-
               $quotes = $this->_quoteFactory
                   ->create()
                   ->getCollection()
                   ->addFieldToFilter('is_active', 1)
                   ->addFieldToFilter('items_count', array('gt' => 0))
                   ->addFieldToFilter('customer_email', array('notnull' => true))
-                  ->addFieldToFilter('created_at', array('from'=>$fromDate, 'to'=>$toDate))
+                  ->addFieldToFilter('created_at', ['lteq' => $fromDate, 'gteq' => $toDate])
                   ->load();
-
-              $this->_getHelper()->log('==> From To', array('from'=>$fromDate, 'to'=>$toDate));
-              $this->_getHelper()->log('==> $quotes', $quotes->count());
 
               if ($quotes->count() > 0) {
                   foreach ($quotes as $quote) {
@@ -89,32 +59,28 @@ class AbandonedCart
                       $data = new \Magento\Framework\DataObject();
                       $data->setCartId($quote->getId());
                       $data->setQuote($quote->getData());
-                      $items = array_map(function($item, $quote) {
-                          $newItem = new \Magento\Framework\DataObject();
-                          $newItem->addData($item->getData());
-                          $newItem->setCartId($quote->getId());
-                          $newItem->setProduct($item->getProduct()->getData());
-                          return $newItem->getData();
-                      }, $quote->getAllItems());
+
+                      $items = array();
+                      foreach ($quote->getAllItems() as $item) {
+                        $newItem = new \Magento\Framework\DataObject();
+                        $newItem->addData($item->getData());
+                        $newItem->setCartId($quote->getId());
+                        $newItem->setProduct($item->getProduct()->getData());
+
+                        $items[] = $newItem->getData();
+                      }
+
                       $data->setItems($items);
                       $data->setQuantity(count($items));
                       $data->setCustomer($customer->getData());
                       $data->setCreatedAt($quote->getData('created_at'));
 
                       $this->_getApi()->sendEvent(Events::ABANDONED_CART, $data->getData());
-                      $this->_getHelper()->log('==> ABANDONED_CART sent', $data->getData());
 
-                      $this->_getHelper()->log(
-                        '==> ABANDONED_CART_ITEM is enabled?',
-                        $this->_getHelper()->isEventEnabled(Events::ABANDONED_CART_ITEM)
-                      );
                       if ($this->_getHelper()->isEventEnabled(Events::ABANDONED_CART_ITEM)) {
                           foreach ($items as $item) {
-                              $item->setCartId($quote->getId());
-                              $item->setCustomer($customer->getData());
-
+                              $item['customer'] = $customer->getData();
                               $this->_getApi()->sendEvent(Events::ABANDONED_CART_ITEM, $item);
-                              $this->_getHelper()->log('==> ABANDONED_CART_ITEM sent', $item);
                           }
                       }
                   }
